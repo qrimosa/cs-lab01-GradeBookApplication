db3bc00757e42f0313ecd8e7dfb97349fe91ab9c
qrimosa <maratiskhakov9@email.com>
2025-03-03 19:53:58 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,28 @@ namespace GradeBook.GradeBooks{
         public RankedGradeBook(string name) : base(name){
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5){
+                throw new InvalidOperationException();
+            }
+            var sortedGrades = Students.Select(s => s.AverageGrade).OrderByDescending(g => g).ToList();
+
+            int rank = sortedGrades.Count(s => s > averageGrade);
+
+            int toptwenty = (int)Math.Ceiling(Students.Count * 0.2);
+
+            if (rank < toptwenty)
+                return 'A';
+            else if (rank < toptwenty * 2) 
+                return 'B';
+            else if (rank < toptwenty * 3) 
+                return 'C';
+            else if (rank < toptwenty * 4) 
+                return 'D';
+            else
+                return 'F';
+
+        }
     }
 }
